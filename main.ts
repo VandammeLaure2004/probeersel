60345e046cab5a43af5b97ed7c61f472365e9a73
VandammeLaure2004 <125573527+VandammeLaure2004@users.noreply.github.com>
2025-03-31 14:38:39 +0200

    hand clap

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    Timer = 300
+})
+input.onSound(DetectedSound.Loud, function () {
+    klappen = !(klappen)
+})
+input.onButtonPressed(Button.B, function () {
+    Timer = 180
+})
+let klappen = false
+let Timer = 0
+input.setSoundThreshold(SoundThreshold.Loud, 128)
+loops.everyInterval(1000, function () {
+    if (klappen == true) {
+        Timer += -1
+    }
+})
 basic.forever(function () {
-	
+    basic.showNumber(Timer)
 })
